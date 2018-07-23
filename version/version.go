e933f4331990080f69a2f15cdd511739db3f4ade
Jeremiah Buddenhagen <git@bitspill.net>
2018-07-23 13:40:29 -0700

    strip `go version` from automatic builds

--- version/version.go
+++ version/version.go
@@ -1,8 +1,16 @@
 package version
 
+import "strings"
+
 var (
 	GitCommitHash = "No Commit Hash"
 	BuildDate     = "No Build Date"
 	BuiltBy       = "Someone"
 	GoVersion     = "Unknown"
 )
+
+func init() {
+	// `go version` includes redundant prefix, strip if exists
+	// go version go1.10.3 windows/amd64
+	GoVersion = strings.TrimPrefix(GoVersion, "go version ")
+}
