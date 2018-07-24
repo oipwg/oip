c462e79601e7dff06a0678256010e849c00f49cc
Jeremiah Buddenhagen <git@bitspill.net>
2018-07-23 19:06:36 -0700

    catch SIGINT/SIGTERM, improve error handling

--- cmd/oipd/main.go
+++ cmd/oipd/main.go
@@ -11,6 +11,9 @@ import (
 	_ "github.com/bitspill/oip/modules"
 	"github.com/bitspill/oip/version"
 	"github.com/davecgh/go-spew/spew"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var FloRPC flo.RPC
@@ -28,30 +31,57 @@ func main() {
 	defer FloRPC.Disconnect()
 
 	rootContext := context.Background()
+	rootContext, cancelRoot := context.WithCancel(rootContext)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-sigChan
+		log.Error("Received signal %s", sig)
+		cancelRoot()
+	}()
 
 	ctx, cancel := context.WithTimeout(rootContext, 10*time.Minute)
 	defer cancel()
 	err := FloRPC.WaitForFlod(ctx, config.MainFlod.Host, config.MainFlod.User, config.MainFlod.Pass)
 	if err != nil {
 		log.Error("Unable to connect to Flod after 10 minutes", logger.Attrs{"host": config.MainFlod.Host, "err": err})
-		panic(err)
+		shutdown(err)
+		return
 	}
 
 	count, err := FloRPC.GetBlockCount()
 	if err != nil {
-		panic(err)
+		log.Error("GetBlockCount failed", logger.Attrs{"err": err})
+		shutdown(err)
+		return
 	}
 
 	log.Info("FLO Block Count: %d", count)
 
-	datastore.Setup(context.TODO())
-
-	lb, err := InitialSync(context.TODO(), count)
+	err = datastore.Setup(rootContext)
 	if err != nil {
-		panic(err)
+		log.Error("datastore setup failed", logger.Attrs{"err": err})
+		shutdown(err)
+		return
+	}
+
+	lb, err := InitialSync(rootContext, count)
+	if err != nil {
+		log.Error("Initial sync failed", logger.Attrs{"err": err})
+		shutdown(err)
+		return
 	}
 
 	if false {
 		spew.Dump(lb)
 	}
+
+	<-rootContext.Done()
+	shutdown(nil)
+	return
+}
+
+func shutdown(err error) {
+	log.Error("Shutting down...")
 }
--- datastore/datastore.go
+++ datastore/datastore.go
@@ -3,6 +3,7 @@ package datastore
 import (
 	"context"
 
+	"fmt"
 	"github.com/pkg/errors"
 	"gopkg.in/olivere/elastic.v6"
 )
@@ -12,22 +13,24 @@ var AutoBulk BulkIndexer
 
 var mappings = make(map[string]string)
 
-func Setup(ctx context.Context) {
+func Setup(ctx context.Context) error {
 
 	var err error
 	client, err = elastic.NewClient()
 	if err != nil {
-		panic("TODO")
+		return errors.Wrap(err, "datastore.setup.newClient")
 	}
 
 	for index, mapping := range mappings {
 		err := createIndex(ctx, index, mapping)
 		if err != nil {
-			panic(err)
+			return errors.Wrap(err, fmt.Sprint("datastore.setup.createIndex", index))
 		}
 	}
 
 	AutoBulk = BeginBulkIndexer()
+
+	return nil
 }
 
 func RegisterMapping(index string, mapping string) error {
