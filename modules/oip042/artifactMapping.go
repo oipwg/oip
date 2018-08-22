f9ad23fe76b6444f2f4a4e6647a5dcab47463db4
Jeremiah Buddenhagen <bitspill+github@bitspill.net>
2018-08-22 17:40:47 -0500

    Fix oip042 mapping

--- modules/oip042/artifactMapping.go
+++ modules/oip042/artifactMapping.go
@@ -7,209 +7,239 @@ const publishOip042ArtifactMapping = `{
     "_doc": {
       "dynamic": "strict",
       "properties": {
-        "details": {
+        "artifact": {
           "properties": {
-            "NBCItaxID": {
-              "type": "long"
-            },
-            "NCBItaxID": {
-              "type": "long"
-            },
-            "artNotes": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "artist": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "date": {
-              "type": "long"
-            },
-            "defocus": {
-              "type": "long"
-            },
-            "dosage": {
-              "type": "long"
-            },
-            "genre": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "institution": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "lab": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "magnification": {
-              "type": "long"
-            },
-            "microscopist": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "roles": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "scopeName": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "sid": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "speciesName": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "strain": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "tiltConstant": {
-              "type": "long"
-            },
-            "tiltMax": {
-              "type": "long"
-            },
-            "tiltMin": {
-              "type": "long"
-            },
-            "tiltSingleDual": {
-              "type": "long"
-            },
-            "tiltStep": {
-              "type": "long"
-            }
-          }
-        },
-        "floAddress": {
-          "type": "keyword",
-          "ignore_above": 256
-        },
-        "info": {
-          "properties": {
-            "description": {
-              "type": "text",
-              "fields": {
-                "keyword": {
-                  "type": "keyword",
-                  "ignore_above": 256
-                }
-              }
-            },
-            "tags": {
-              "type": "text",
-              "fields": {
-                "keyword": {
-                  "type": "keyword",
-                  "ignore_above": 256
-                }
-              }
-            },
-            "title": {
-              "type": "text",
-              "fields": {
-                "keyword": {
-                  "type": "keyword",
-                  "ignore_above": 256
-                }
-              }
-            },
-            "year": {
-              "type": "date",
-              "format": "year"
-            }
-          }
-        },
-        "payment": {
-          "properties": {
-            "fiat": {
-              "type": "keyword",
-              "ignore_above": 256
-            },
-            "maxdisc": {
-              "type": "long"
-            },
-            "promoter": {
-              "type": "long"
-            },
-            "retailer": {
-              "type": "long"
-            },
-            "scale": {
-              "type": "keyword",
-              "ignore_above": 256
-            }
-          }
-        },
-        "signature": {
-          "type": "text",
-          "index": false
-        },
-        "storage": {
-          "properties": {
-            "files": {
+            "details": {
               "properties": {
-                "cType": {
-                  "type": "keyword",
-                  "ignore_above": 256
-                },
-                "dname": {
-                  "type": "keyword",
-                  "ignore_above": 256
-                },
-                "fNotes": {
-                  "type": "keyword",
-                  "ignore_above": 256
-                },
-                "fname": {
-                  "type": "keyword",
-                  "ignore_above": 256
-                },
-                "fsize": {
+                "NBCItaxID": {
                   "type": "long"
                 },
-                "software": {
+                "NCBItaxID": {
+                  "type": "long"
+                },
+                "artNotes": {
                   "type": "keyword",
                   "ignore_above": 256
                 },
-                "subtype": {
+                "artist": {
                   "type": "keyword",
                   "ignore_above": 256
                 },
-                "type": {
+                "date": {
+                  "type": "long"
+                },
+                "defocus": {
+                  "type": "long"
+                },
+                "dosage": {
+                  "type": "long"
+                },
+                "genre": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "institution": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "lab": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "magnification": {
+                  "type": "long"
+                },
+                "microscopist": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "roles": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "scopeName": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "sid": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "speciesName": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "strain": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "tiltConstant": {
+                  "type": "long"
+                },
+                "tiltMax": {
+                  "type": "long"
+                },
+                "tiltMin": {
+                  "type": "long"
+                },
+                "tiltSingleDual": {
+                  "type": "long"
+                },
+                "tiltStep": {
+                  "type": "long"
+                }
+              }
+            },
+            "floAddress": {
+              "type": "keyword",
+              "ignore_above": 256
+            },
+            "info": {
+              "properties": {
+                "description": {
+                  "type": "text",
+                  "fields": {
+                    "keyword": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    }
+                  }
+                },
+                "tags": {
+                  "type": "text",
+                  "fields": {
+                    "keyword": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    }
+                  }
+                },
+                "title": {
+                  "type": "text",
+                  "fields": {
+                    "keyword": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    }
+                  }
+                },
+                "year": {
+                  "type": "date",
+                  "format": "year"
+                }
+              }
+            },
+            "payment": {
+              "properties": {
+                "fiat": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "maxdisc": {
+                  "type": "long"
+                },
+                "promoter": {
+                  "type": "long"
+                },
+                "retailer": {
+                  "type": "long"
+                },
+                "scale": {
                   "type": "keyword",
                   "ignore_above": 256
                 }
               }
             },
-            "location": {
+            "signature": {
+              "type": "text",
+              "index": false
+            },
+            "storage": {
+              "properties": {
+                "files": {
+                  "properties": {
+                    "cType": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    },
+                    "dname": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    },
+                    "fNotes": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    },
+                    "fname": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    },
+                    "fsize": {
+                      "type": "long"
+                    },
+                    "software": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    },
+                    "subtype": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    },
+                    "type": {
+                      "type": "keyword",
+                      "ignore_above": 256
+                    }
+                  }
+                },
+                "location": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                },
+                "network": {
+                  "type": "keyword",
+                  "ignore_above": 256
+                }
+              }
+            },
+            "subtype": {
               "type": "keyword",
               "ignore_above": 256
             },
-            "network": {
+            "timestamp": {
+              "type": "date",
+              "format": "epoch_second"
+            },
+            "type": {
               "type": "keyword",
               "ignore_above": 256
+            },
+            "meta": {
+              "properties": {
+                "block": {
+                  "type": "long"
+                },
+                "block_hash": {
+                  "type": "keyword",
+                  "ignore_above": 64
+                },
+                "deactivated": {
+                  "type": "boolean"
+                },
+                "time": {
+                  "type": "date",
+                  "format": "epoch_second"
+                },
+                "txid": {
+                  "type": "keyword",
+                  "ignore_above": 64
+                },
+                "tx": {
+                  "type": "object",
+                  "enabled": false
+                }
+              }
             }
           }
-        },
-        "subtype": {
-          "type": "keyword",
-          "ignore_above": 256
-        },
-        "timestamp": {
-          "type": "date",
-          "format": "epoch_second"
-        },
-        "type": {
-          "type": "keyword",
-          "ignore_above": 256
         },
         "meta": {
           "properties": {
@@ -223,17 +253,21 @@ const publishOip042ArtifactMapping = `{
             "deactivated": {
               "type": "boolean"
             },
+            "signature": {
+              "type": "text",
+              "index": false
+            },
             "time": {
               "type": "date",
               "format": "epoch_second"
             },
-            "txid": {
-              "type": "keyword",
-              "ignore_above": 64
-            },
             "tx": {
               "type": "object",
               "enabled": false
+            },
+            "txid": {
+              "type": "keyword",
+              "ignore_above": 64
             }
           }
         }
