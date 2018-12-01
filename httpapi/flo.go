6530e34cf9641203ba9f4c9584b550e1a368232c
Jeremiah Buddenhagen <git@bitspill.net>
2018-11-30 16:37:56 -0800

    add get latest floData api endpoint

--- httpapi/flo.go
+++ httpapi/flo.go
@@ -16,7 +16,8 @@ func init() {
 	router.HandleFunc("/floData/search", handleFloDataSearch).Queries("q", "{query}", "limit", "{limit:[0-9]+}")
 	router.HandleFunc("/floData/search", handleFloDataSearch).Queries("q", "{query}")
 	router.HandleFunc("/floData/get/{id:[a-f0-9]+}", handleGetFloData)
-
+	router.HandleFunc("/floData/latest/{limit:[0-9]+}", handleFloDataLatest).Queries("coinbase", "{coinbase}")
+	router.HandleFunc("/floData/latest/{limit:[0-9]+}", handleFloDataLatest)
 }
 
 func handleGetFloData(w http.ResponseWriter, r *http.Request) {
@@ -41,7 +42,7 @@ func handleGetFloData(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		log.Error("elastic search failed", logger.Attrs{"err": err})
-		RespondJSON(w, 500, map[string]interface{} {
+		RespondJSON(w, 500, map[string]interface{}{
 			"error": "database error",
 		})
 	}
@@ -51,8 +52,8 @@ func handleGetFloData(w http.ResponseWriter, r *http.Request) {
 		sources[k] = v.Source
 	}
 
-	RespondJSON(w, http.StatusOK, map[string]interface{} {
-		"total": results.Hits.TotalHits,
+	RespondJSON(w, http.StatusOK, map[string]interface{}{
+		"total":   results.Hits.TotalHits,
 		"results": sources,
 	})
 }
@@ -83,6 +84,58 @@ func handleFloDataSearch(w http.ResponseWriter, r *http.Request) {
 	log.Info(searchQuery)
 	fsc := elastic.NewFetchSourceContext(true).
 		Include("tx.floData", "tx.txid", "tx.time", "tx.blockhash", "tx.size")
+
+	results, err := datastore.Client().
+		Search(datastore.Index("transactions")).
+		Type("_doc").
+		Query(q).
+		Size(int(size)).
+		Sort("tx.time", false).
+		FetchSourceContext(fsc).
+		Do(context.TODO())
+
+	if err != nil {
+		log.Error("elastic search failed", logger.Attrs{"err": err})
+		RespondJSON(w, 500, map[string]interface{}{
+			"error": "database error",
+		})
+		return
+	}
+
+	sources := make([]interface{}, len(results.Hits.Hits))
+	for k, v := range results.Hits.Hits {
+		sources[k] = v.Source
+	}
+
+	RespondJSON(w, http.StatusOK, map[string]interface{}{
+		"count":   len(results.Hits.Hits),
+		"total":   results.Hits.TotalHits,
+		"results": sources,
+	})
+}
+
+func handleFloDataLatest(w http.ResponseWriter, r *http.Request) {
+	var opts = mux.Vars(r)
+
+	lim, _ := opts["limit"]
+	size, _ := strconv.ParseInt(lim, 10, 0)
+	if size <= 0 || size > 1000 {
+		size = -1
+	}
+
+	q := elastic.NewBoolQuery().Must(
+		elastic.NewExistsQuery("tx.floData"),
+	)
+
+	if c, ok := opts["coinbase"]; ok {
+		coinbase, _ := strconv.ParseBool(c)
+		if coinbase == false {
+			q.Must(elastic.NewTermQuery("is_coinbase", coinbase))
+		}
+	}
+
+	fsc := elastic.NewFetchSourceContext(true).
+		Include("tx.floData", "tx.txid", "tx.time", "tx.blockhash", "tx.size", "is_coinbase")
 
 	results, err := datastore.Client().
 		Search(datastore.Index("transactions")).
