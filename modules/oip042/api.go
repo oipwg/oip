93892d2bcfdf608c9a48ce1f28e1fff94606dfaf
cmills <cmills@mediciventures.com>
2019-05-15 15:54:22 -0600

    MLG-1202 Added REST endpoints to retrieve OIP042 records according to transactionID and originalTxid.

--- modules/oip042/api.go
+++ modules/oip042/api.go
@@ -10,10 +10,14 @@ import (
 )
 
 var artRouter = httpapi.NewSubRoute("/oip042/artifact")
+var recordRouter = httpapi.NewSubRoute("/oip042/record")
 
 func init() {
 	artRouter.HandleFunc("/get/latest", handleLatest).Queries("nsfw", "{nsfw}")
 	artRouter.HandleFunc("/get/latest", handleLatest)
+	recordRouter.HandleFunc("/get/{editRecordTxid}", handleGet)
+	recordRouter.HandleFunc("/get/{originalTxid}/latest", handleGetLatestEdit)
+	recordRouter.HandleFunc("/get/{originalTxid}/version/{editRecordTxid}", handleGets)
 }
 
 var (
@@ -47,4 +51,109 @@ func handleLatest(w http.ResponseWriter, r *http.Request) {
 		o42ArtifactFsc,
 	)
 	httpapi.RespondSearch(w, searchService)
+}
+
+/**
+This method will return the version of the record requested with all edits applied. So, the most recent version of the record.
+*/
+func handleGet(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	editRecordTxid := vars["editRecordTxid"]
+	var opts = mux.Vars(request)
+
+	query := elastic.NewBoolQuery().Must(
+		elastic.NewTermQuery("meta.deactivated", false),
+		elastic.NewTermQuery("meta.txid", editRecordTxid),
+	)
+
+	if n, ok := opts["nsfw"]; ok {
+		nsfw, _ := strconv.ParseBool(n)
+		if !nsfw {
+			query.MustNot(elastic.NewTermQuery("artifact.info.nsfw", true))
+		}
+		log.Info("nsfw: %t", nsfw)
+	}
+
+	searchService := httpapi.BuildCommonSearchService(
+		request.Context(),
+		[]string{oip042ArtifactIndex},
+		query,
+		[]elastic.SortInfo{
+			{Field: "meta.time", Ascending: false},
+			{Field: "meta.txid", Ascending: true},
+		},
+		o42ArtifactFsc,
+	)
+	httpapi.RespondSearch(response, searchService)
+}
+
+/**
+This method will return the version of the record requested with all edits applied. So, the most recent version of the record.
+*/
+func handleGetLatestEdit(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	originalTxid := vars["originalTxid"]
+	var opts = mux.Vars(request)
+
+	query := elastic.NewBoolQuery().Must(
+		elastic.NewTermQuery("meta.deactivated", false),
+		elastic.NewTermQuery("meta.originalTxid", originalTxid),
+		elastic.NewTermQuery("meta.latest", true),
+	)
+
+	if n, ok := opts["nsfw"]; ok {
+		nsfw, _ := strconv.ParseBool(n)
+		if !nsfw {
+			query.MustNot(elastic.NewTermQuery("artifact.info.nsfw", true))
+		}
+		log.Info("nsfw: %t", nsfw)
+	}
+
+	searchService := httpapi.BuildCommonSearchService(
+		request.Context(),
+		[]string{oip042ArtifactIndex},
+		query,
+		[]elastic.SortInfo{
+			{Field: "meta.time", Ascending: false},
+			{Field: "meta.txid", Ascending: true},
+		},
+		o42ArtifactFsc,
+	)
+	httpapi.RespondSearch(response, searchService)
+}
+
+/**
+This method will return the record requested with the version requested. The version will be specified using the transaction ID.
+*/
+func handleGets(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	originalTxid := vars["originalTxid"]
+	editRecordTxid := vars["editRecordTxid"]
+	var opts = mux.Vars(request)
+
+	query := elastic.NewBoolQuery().Must(
+		elastic.NewTermQuery("meta.deactivated", false),
+		elastic.NewTermQuery("meta.originalTxid", originalTxid),
+		elastic.NewTermQuery("meta.txid", editRecordTxid),
+	)
+
+	if n, ok := opts["nsfw"]; ok {
+		nsfw, _ := strconv.ParseBool(n)
+		if !nsfw {
+			query.MustNot(elastic.NewTermQuery("artifact.info.nsfw", true))
+		}
+		log.Info("nsfw: %t", nsfw)
+	}
+
+	searchService := httpapi.BuildCommonSearchService(
+		request.Context(),
+		[]string{oip042ArtifactIndex},
+		query,
+		[]elastic.SortInfo{
+			{Field: "meta.time", Ascending: false},
+			{Field: "meta.txid", Ascending: true},
+		},
+		o42ArtifactFsc,
+	)
+	httpapi.RespondSearch(response, searchService)
 }
