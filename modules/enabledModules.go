c87ed903650b9acec41c220a372f816de2e31ddf
Jeremiah Buddenhagen <git@bitspill.net>
2020-01-14 18:36:35 -0800

    remove floDataDump

--- modules/enabledModules.go
+++ modules/enabledModules.go
@@ -3,7 +3,6 @@ package modules
 import (
 	_ "github.com/oipwg/oip/modules/alexandriaMedia"
 	_ "github.com/oipwg/oip/modules/aternaLove"
-	_ "github.com/oipwg/oip/modules/floDataDump"
 	_ "github.com/oipwg/oip/modules/flotorizer"
 	_ "github.com/oipwg/oip/modules/historian"
 	_ "github.com/oipwg/oip/modules/oip"
